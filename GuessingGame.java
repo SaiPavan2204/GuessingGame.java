2e1f9a725c23594530d3af587726c31935139a86
SaiPavan2204 <ravvasaipavan@gmail.com>
2026-03-11 10:42:19 +0530

    Added Guessing Game Project

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,36 +1,33 @@
-import java.util.Random;
 import java.util.Scanner;
+import java.util.Random;
 
 public class GuessingGame {
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        Random random = new Random();
+        Random rand = new Random();
 
-        int secretNumber = random.nextInt(100) + 1;
+        int secretNumber = rand.nextInt(100) + 1; 
         int guess;
-        int attempts = 0;
 
-        System.out.println("===== NUMBER GUESSING GAME =====");
+        System.out.println("=== Number Guessing Game ===");
         System.out.println("Guess a number between 1 and 100");
 
-        while (true) {  
+        while (true) {
 
             System.out.print("Enter your guess: ");
             guess = sc.nextInt();
 
-            attempts++;
-
-            if (guess == secretNumber) {
-                System.out.println("Correct! You guessed it in " + attempts + " attempts.");
-                break; 
+            if (guess > secretNumber) {
+                System.out.println("Too High! Try again.");
             }
-            else if (guess > secretNumber) {
-                System.out.println("Too high! Try again.");
+            else if (guess < secretNumber) {
+                System.out.println("Too Low! Try again.");
             }
             else {
-                System.out.println("Too low! Try again.");
+                System.out.println("Correct! You guessed the number.");
+                break;
             }
         }
 
